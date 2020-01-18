87fed06a0d2f90cd1a56b4256d34512c77316601
baltikastro <darragino1@gmail.com>
2020-01-18 16:49:19 +0100

    Create Es6.js

---
+++ Es6.js
@@ -0,0 +1,31 @@
+const pets = [
+  { name: "Max", type: "dog", bornOn: 2018 },
+  { name: "Angel", type: "cat", bornOn: 2015 },
+  { name: "Jasper", type: "dog", bornOn: 2016 }
+];
+
+const getAge = pet => {
+  return new Date().getFullYear() - pet.bornOn;
+}
+
+const petsWithAge = [];
+
+
+
+pets.forEach((pet) => {
+  pet.age = getAge(pet);
+  petsWithAge.push(pet);
+}
+);
+console.log(petsWithAge);
+
+
+
+const dogs = pets.filter((pet) => { return pet.type == "dog" })
+
+console.log(dogs);
+
+
+const jasper = dogs.find((dog) => { return dog.name == "Jasper" })
+
+console.log(`Jasper is ${jasper.age} years old`);
